3ee816e7c96b4b5f1b8f630fdbe1855c4371fc26
vienlemai <vienlm@ilucians.com>
2014-06-29 22:18:46 +0700

    test commiter

--- app/config/database.sample.php
+++ app/config/database.sample.php
@@ -4,17 +4,12 @@ return array(
     'fetch' => PDO::FETCH_CLASS,
     'default' => 'mysql',
     'connections' => array(
-        'sqlite' => array(
-            'driver' => 'sqlite',
-            'database' => __DIR__ . '/../database/production.sqlite',
-            'prefix' => '',
-        ),
         'mysql' => array(
             'driver' => 'mysql',
             'host' => 'localhost',
-            'database' => 'tasoha',
-            'username' => 'root',
-            'password' => '123456',
+            'database' => '',
+            'username' => '',
+            'password' => '',
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
