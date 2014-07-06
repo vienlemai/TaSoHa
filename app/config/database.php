2ca646e5033da461a4533a3cdf76dfde48f384ae
vienlemai <vienlm@ilucians.com>
2014-07-06 16:55:27 +0700

    up

--- app/config/database.php
+++ app/config/database.php
@@ -7,9 +7,9 @@ return array(
         'mysql' => array(
             'driver' => 'mysql',
             'host' => 'localhost',
-            'database' => 'tasoha',
+            'database' => 'tasoha-dev',
             'username' => 'root',
-            'password' => '',
+            'password' => '123456',
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
--- assets/js/member.js
+++ assets/js/member.js
@@ -4,49 +4,35 @@ jQuery(document).ready(function() {
         dragAndDrop: false
     });
 
-    $('.node').on('click', function() {
-        var dataUrl = $('#url-show-member').val();
-        var memberId = $(this).find('.member-id').val();
-        var dataUrl = dataUrl.replace('-1', memberId);
-        $.ajax({
-            url: dataUrl,
-            type: 'get',
-            data: null,
-            success: function(result) {
-                $(result).modal();
-            },
-            error: function() {
-                alert('Đã có lỗi xảy ra, vui lòng thử lại');
-            }
-        });
-    });
-//    var timer;
-//    $('.members-tree-wrap .node').hover(function() {
-//        $_this = $(this);
-//        if (timer) {
-//            clearTimeout(timer);
-//            timer = null;
-//        }
-//        timer = setTimeout(function() {
-//            var html = '<div class="popover" role="tooltip"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div></div>';
-//            $_this.popover({
-//                template: html
-//            });
-//            console.log('hovered');
-//        }, 700);
-//    }, function() {
-//        console.log('out');
+//    $('.node').on('click', function() {
+//        var dataUrl = $('#url-show-member').val();
+//        var memberId = $(this).find('.member-id').val();
+//        var dataUrl = dataUrl.replace('-1', memberId);
+//        $.ajax({
+//            url: dataUrl,
+//            type: 'get',
+//            data: null,
+//            success: function(result) {
+//                $(result).modal();
+//            },
+//            error: function() {
+//                alert('Đã có lỗi xảy ra, vui lòng thử lại');
+//            }
+//        });
 //    });
 
+    /* Handle mouse hover on tree nodes */
     $('.members-tree-wrap .node').each(function() {
         var $_this = $(this);
         var $_input = $(this).find('input');
 //        var template = '<div class="popover" role="tooltip"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div></div>';
+        var dismissBtn = '<button type="button" class="close" data-toggle="popover">×</button>';
+        var detailLink = '<br><a href="#" class="btn btn-default btn-xs pull-right btn-view-member-detail"><i class="fa fa-eye"></i> Chi tiết</a>';
         var content = 'Họ tên: ' + $_input.data('fullname');
         var options = {
             title: 'Thông tin thành viên',
             content: content,
-            placement: 'right',
+            placement: 'top',
             delay: {
                 show: 500,
                 hide: 100
@@ -55,15 +41,43 @@ jQuery(document).ready(function() {
         };
         $_this.popover(options);
 
-        $_this.hover(function() {
-            $_this.popover('show');
+        $_this.on('mouseover', function() {
+            if (!$_this.hasClass('popovered')) {
+                $_this.popover('show').addClass('popovered');
+                $_this.next('.popover').find('.popover-title').append(dismissBtn);
+                $_this.next('.popover').find('.popover-content').append(detailLink);
+            }
         });
 
         $_this.on('show.bs.popover', function() {
-            $('.members-tree-wrap .node').not(this).popover('hide');
+            $('.members-tree-wrap .node').not(this).popover('hide').removeClass('popovered');
         });
-
     });
 
+    // Handler close popover button
+    $(document).on('click', '.popover .close', function() {
+        $(this).closest('.popover').prev('.node').popover('hide');
+    });
 
+    //
+    $(document).on('click', '.popover .btn-view-member-detail', function() {
+        var $_node = $(this).closest('.popover').prev('.node');
+        var dataUrl = $('#url-show-member').val();
+        var memberId = $_node.find('.member-id').val();
+        var dataUrl = dataUrl.replace('-1', memberId);
+        $.ajax({
+            url: dataUrl,
+            type: 'get',
+            data: null,
+            success: function(result) {
+                $_node.popover('hide');
+                $(result).modal();
+                
+            },
+            error: function() {
+                alert('Đã có lỗi xảy ra, vui lòng thử lại');
+            }
+        });
+        return false;
+    });
 });
