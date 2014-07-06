334bd6097fb978aa9d351a79487efd589d9d80e3
homeworkfree <lemaibk08@gmail.com>
2014-07-06 19:54:27 +0700

    db

---
+++ app/config/database.php
@@ -0,0 +1,19 @@
+<?php
+
+return array(
+    'fetch' => PDO::FETCH_CLASS,
+    'default' => 'mysql',
+    'connections' => array(
+        'mysql' => array(
+            'driver' => 'mysql',
+            'host' => '127.10.129.2',
+            'database' => 'tasoha',
+            'username' => 'adminMl4uygt',
+            'password' => 'PjhCemc6Aysp',
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+        )
+    ),
+    'migrations' => 'migrations'
+);
