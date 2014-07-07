ee73197621e752e3bdc4a775174451cc683d5981
homeworkfree <lemaibk08@gmail.com>
2014-07-07 21:40:30 +0700

    add local enviroment

---
+++ bootstrap/environment.php
@@ -0,0 +1,7 @@
+<?php
+
+$env = $app->detectEnvironment(array(
+    'local' => array('SONY-VAIO'),
+    //'staging' => array('staging-server'),
+    //'production' => array('production-server')
+    ));
